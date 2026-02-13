2a0fa28df629eabdd8b760d2cb53669b94d1d40f
Adam Adifa <adamabdi.al.a@gmail.com>
2026-02-13 15:44:27 +0700

    update

--- resources/views/marketing/laporan/penjualan_formatstandar_cetak.blade.php
+++ resources/views/marketing/laporan/penjualan_formatstandar_cetak.blade.php
@@ -167,7 +167,7 @@
                                 @if ($k == 0)
                                     <td rowspan="{{ count($val) }}">{{ formatIndo($d->tanggal) }}</td>
                                     <!-- <td rowspan="{{ count($val) }}">{{ !empty($d->no_fak_new) ? $d->no_fak_new : $d->no_faktur }}</td> -->
-                                     <td>{{ $d->no_faktur }}</td>
+                                    <td rowspan="{{ count($val) }}">{{ $d->no_faktur }}</td>
                                     <td rowspan="{{ count($val) }}">{{ $d->kode_pelanggan }}</td>
                                     <td rowspan="{{ count($val) }}">{{ $d->nama_pelanggan }}</td>
                                     <td rowspan="{{ count($val) }}">{{ $d->nama_salesman }}</td>
